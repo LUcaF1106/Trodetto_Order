4a49c41ff4f44e06f6871717e5dc99cf685d86e2
LUcaF1106 <luca06.frasson@gmail.com>
2026-06-03 23:23:54 +0200

    Update product.service.ts

--- src/app/common/service/product/product.service.ts
+++ src/app/common/service/product/product.service.ts
@@ -1,13 +1,18 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ProductJson } from '../../interface/product_json';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
-  private http: HttpClient = inject(HttpClient);
+  private http = inject(HttpClient);
+  private platformId = inject(PLATFORM_ID);
 
   getProdotti(): Observable<ProductJson[]> {
+    if (!isPlatformBrowser(this.platformId)) {
+      return of([]); // lato server restituisce array vuoto
+    }
     return this.http.get<ProductJson[]>('/product.json');
   }
 }
