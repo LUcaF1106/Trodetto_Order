047d556ccdec169bf8ef211b58f7016573252872
LUcaF1106 <luca06.frasson@gmail.com>
2026-06-04 11:03:09 +0200

    fix: usa fetch nativo lato server, rimuove xhr2

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,5 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { appConfig } from './app.config';
 import { routes } from './app.routes';
@@ -7,6 +8,7 @@ import { routes } from './app.routes';
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    provideHttpClient(withFetch()),
     importProvidersFrom(
       RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })
     ),
