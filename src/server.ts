20d11b13095a276da02b26d567f5ef606be9906d
LUcaF1106 <luca06.frasson@gmail.com>
2026-06-04 11:55:08 +0200

    fix: trustProxyHeaders per Workers

--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,9 @@ const angularApp = new AngularAppEngine();
 
 export default {
   async fetch(request: Request): Promise<Response> {
-    // Rimuove l'header host che causa problemi con AngularAppEngine
-    const headers = new Headers(request.headers);
-    headers.delete('host');
-
-    const newRequest = new Request(request.url, {
-      method: request.method,
-      headers,
-      body: request.body,
-      redirect: request.redirect,
+    const response = await angularApp.handle(request, {
+      server: { trustProxyHeaders: true }
     });
-
-    const response = await angularApp.handle(newRequest);
     return response ?? new Response('Not found', { status: 404 });
   },
 };
